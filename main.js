a958351c61f233350997018ca25b99366db4aa3a
DevLucas <luksilvafe@gmail.com>
2024-07-04 15:14:04 -0300

    Calculadora que calcula

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, Menu} = require('electron')
+const { app, BrowserWindow, Menu, shell } = require('electron')
 // const { BrowserWindow } = require('electron')
 
 
@@ -8,10 +8,52 @@ app.on('ready', () => {
     janelaPrincipal = new BrowserWindow({
         width: 1200,
         height: 800,
-        webPreferences:{
-        
+
+        webPreferences: {
+
         }
     })
     // janelaPrincipal.loadURL(`file://${__dirname}/app/index.html`)
     janelaPrincipal.loadFile('app/index.html')
-})
+    Menu.setApplicationMenu(Menu.buildFromTemplate(Template))
+});
+
+const Template = [
+    {
+        label: 'Arquivo',
+        submenu: [
+            {
+                label: 'Sair',
+                click: () => app.quit(),
+                accelerator: 'Alt+F4'
+            },
+
+
+        ]
+
+    },
+
+    {
+        label: 'Sobre',
+        click: () => janelaSobre()
+    },
+
+    {
+        label: 'Ajuda',
+        submenu: [
+            {
+                label: 'Documentação',
+                click: () => shell.openExternal('https://drive.google.com/file/d/1RA3foohoitv_mNHi3zYOcWuK0fzeBXdF/view?usp=sharing')
+            }
+        ]
+    }
+]
+
+const janelaSobre = () => {
+    const sobre = new BrowserWindow({
+        width: 500,
+        height: 300,
+        autoHideMenuBar: true
+    });
+    sobre.loadFile('app/sobre.html')
+}
